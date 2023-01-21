fc1b5ca206190d703c875e34cab8a8e4a703a50b
Vishaal <vishaalc1@gmail.com>
2023-01-21 19:22:32 +0530

    Add project files.

---
+++ TodoListApp/TodoListApp/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace TodoListApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void HandleClick(object sender, EventArgs args)
+        {
+            Console.WriteLine("Clicked");
+        }
+
+        public void HandleTextChanged(object sender, TextChangedEventArgs args)
+        {
+            Console.WriteLine(args.NewTextValue);
+
+        }
+        public void HandleEnterPress(object sender, EventArgs args)
+        {
+            Console.WriteLine("Enter Pressed");
+            Label newTodo = new Label();
+            newTodo.Text = InputField.Text;
+            newTodo.FontSize = 20;
+            ToDoList.Children.Add(newTodo);
+            InputField.Text = "";
+        }
+    }
+}
